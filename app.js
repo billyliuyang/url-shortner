26a90c4ea895f6538be037c7e88d1bfe7a8069e2
Billy <yliu179@sheffield.ac.uk>
2017-10-12 12:26:15 +0100

    POST /shorten

--- app.js
+++ app.js
@@ -1,22 +1,56 @@
 var http = require('http');
 var url = require('url');
 var fs = require('fs');
+var RandExp = require('randexp');
 
 http.createServer(function (req, res) {
+	//If the request is a POST
 	if (req.method == 'POST') {
 		var q = url.parse(req.url, true);
-		var requestData;
+		var requestData = "";
+		//If the POST requst aims to /shorten
 		if(q.pathname == '/shorten'){
 			req.on('data',function(data){
-				requestData = data;
+				requestData += data;
 			});
 			req.on('end',function(){
-				console.log(JSON.parse(requestData));
+				var jsonData = JSON.parse(requestData)
+				//If the url is provided
+				if(jsonData.url != ""){
+					//If the shortcode is provided
+					if(jsonData.shortcode != ""){
+						var regex = new RegExp("^[0-9a-zA-Z_]{4,}$");
+						//If the provided shortcode meet the regex
+						if(regex.test(jsonData.shortcode)){
+							console.log("201 Created");
+							console.log("Content-Type: " + '"' + req.headers['content-type'] + '"');
+							console.log("");
+							console.log("{")
+        					console.log('	"shortcode": ' + '"' + jsonData.shortcode + '"');
+        					console.log("}");
+        				//If the provided shortcode doesn't meet the regex
+						}else{
+							console.log("422 The shortcode fails to meet the following regexp: ^[0-9a-zA-Z_]{4,}$");
+						}
+						
+        			//If the shortcode is null
+					}else{
+						console.log(new RandExp("^[0-9a-zA-Z_]{6}$").gen());
+					}
+				//If the url is null
+				}else{
+					console.log("400 URL is not present")
+				}
 			});
+
+
 			res.end();
+		//If the POST request doesn't point to /shorten
 		}else{
 			console.log("Error: Invalid POST request.");
 		}
+
+	//If the request is a GET
 	}else{
 		console.log("GET request");
 		res.writeHead(200, "OK", {'Content-Type': 'text/plain'});
--- request.js
+++ request.js
@@ -5,13 +5,15 @@ var requestData = JSON.parse(fs.readFileSync('./requestData.json', 'utf8'));
 request.post({
 	url: "http://localhost:8080/shorten",
     method: "POST",
-    headers: {
-        "content-type": "application/json",
-        },
+    headers: {"content-type": "application/json"},
     json: requestData
 	},function (err, res, body) {
         console.log("POST /shorten");
         console.log('Content-Type: "application/json"');
-        console.log(requestData);
+        console.log("");
+        console.log("{");
+        console.log('	"url": ' + '"' + requestData.url + '"');
+        console.log('	"shortcode": ' + '"' + requestData.shortcode + '"');
+        console.log("}");
     }
 );
