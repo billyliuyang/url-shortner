11728c4bae28ed1c39be92a3df2e399533dd79ca
Billy <yliu179@sheffield.ac.uk>
2017-10-12 02:20:13 +0100

    post /shorten request

--- app.js
+++ app.js
@@ -1,1 +1,54 @@
-this is main application
+var http = require('http');
+var url = require('url');
+var fs = require('fs');
+
+http.createServer(function (req, res) {
+	if (req.method == 'POST') {
+		var q = url.parse(req.url, true);
+		var requestData;
+		if(q.pathname == '/shorten'){
+			req.on('data',function(data){
+				requestData = data;
+			});
+			req.on('end',function(){
+				console.log(JSON.parse(requestData));
+			});
+			res.end();
+		}else{
+			console.log("Error: Invalid POST request.");
+		}
+	}else{
+		console.log("GET request");
+		res.writeHead(200, "OK", {'Content-Type': 'text/plain'});
+		res.end();
+	}
+  
+
+
+  // if (req.method == 'POST'){
+	 //  var q = url.parse(req.url, true);
+	 //  if (q){
+
+	 //  }else{
+
+	 //  }
+
+
+	 //  var filename = "." + q.pathname;
+	 //  fs.readFile(filename, function(err, data) {
+	 //    if (err) {
+	 //      res.writeHead(404, {'Content-Type': 'text/html'});
+	 //      console.log(err);
+	 //      return res.end("404 Not Found");
+	 //    } 
+	 //    res.writeHead(200, {'Content-Type': 'text/html'});
+	 //    res.write(data);
+	 //    console.log(data);
+	 //    return res.end();
+	 //  });
+  // } else {
+  // 		console.log("get request");
+  //   	res.writeHead(200, "OK", {'Content-Type': 'text/plain'});
+  //   	res.end();
+  // };
+}).listen(8080);
--- request.js
+++ request.js
@@ -1,1 +1,17 @@
-this is used to send POST request
+var request = require('request');
+var fs = require('fs');
+var requestData = JSON.parse(fs.readFileSync('./requestData.json', 'utf8'));
+
+request.post({
+	url: "http://localhost:8080/shorten",
+    method: "POST",
+    headers: {
+        "content-type": "application/json",
+        },
+    json: requestData
+	},function (err, res, body) {
+        console.log("POST /shorten");
+        console.log('Content-Type: "application/json"');
+        console.log(requestData);
+    }
+);
